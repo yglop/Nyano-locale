655b1b02a9ebfc250ba6164a323452faaf753477
yglop <95057024+yglop@users.noreply.github.com>
2023-05-23 11:44:32 +0500

    Update new_lines_finder.py

--- scripts/yglop/new_lines_finder.py
+++ scripts/yglop/new_lines_finder.py
@@ -1,16 +1,9 @@
 ### how to use: ToDo
 
-arumoon_dir = './ru-RU-aru/'
-old_nyanotrasen_dir = './nyanotrasen/'
-new_nyanotrasen_dir = './ru-RU-nya/'
+old_nyanotrasen_dir = './ru-RU-old/'
+new_nyanotrasen_dir = './ru-RU-new/'
 
 import os
- 
-# writes file dirs for aromoon locale
-arumoon_files = []
-for root, dirs, files in os.walk(arumoon_dir):
-    for filename in files:
-        arumoon_files.append(os.path.join(root, filename))
 
 # writes file dirs for nyano locale
 old_nyanotrasen_files = []
@@ -52,15 +45,6 @@ def parser(f):
             data[key] = val
     return data
 
-
-# writes data to memory from aromoon locale
-data_arumoon = {}
-for _ in arumoon_files:
-    with open(_) as f:
-        parsed_data = parser(f)
-        for k, v in parsed_data.items():
-            data_arumoon[k] = v
-
 # writes data to memory from already translated nyano locale
 data_old_nya = {}
 for _ in old_nyanotrasen_files:
@@ -75,32 +59,17 @@ for _ in new_nyanotrasen_files:
     with open(_) as f:
         data_new_nya = parser(f)
         
-        addition = _.split('ru-RU-nya')[1]
+        addition = _.split('ru-RU-new')[1]
         
         # setting up working dirs
-        base_dir = './autotranslate/ru-RU/'        
+        base_dir = './ru-RU-old/new_data/'        
         file_name = addition.split('/')[-1]
         work_dir = addition[:len(file_name) * -1]
-        aru = 'arumoon/'
-        nya = 'nyanotrasen/' 
-        new_data = 'new_data/'
 
         # writes folder & files in "./autotranslate/ru-RU/"
         for k in data_new_nya:
-            if k in data_arumoon:
-                data_new_nya[k] = data_arumoon[k]
-                if  not os.path.exists(base_dir + aru + work_dir):
-                    os.makedirs(base_dir + aru + work_dir)
-                with open(base_dir + aru + work_dir + file_name, 'a') as f0:
-                    print(k, ' =', data_new_nya[k], file=f0, end='', sep='')
-            elif k in data_old_nya:
-                data_new_nya[k] = data_old_nya[k]
-                if not os.path.exists(base_dir + nya + work_dir):
-                    os.makedirs(base_dir + nya + work_dir)
-                with open(base_dir + nya + work_dir + file_name, 'a') as f0:
-                    print(k, ' =', data_new_nya[k], file=f0, end='', sep='') 
-            else:
-                if not os.path.exists(base_dir + new_data + work_dir):
-                    os.makedirs(base_dir + new_data + work_dir)
-                with open(base_dir + new_data + work_dir + file_name, 'a') as f0:
+            if k not in data_old_nya:
+                if not os.path.exists(base_dir + work_dir):
+                    os.makedirs(base_dir+ work_dir)
+                with open(base_dir + work_dir + file_name, 'a') as f0:
                     print(k, ' =', data_new_nya[k], file=f0, end='', sep='')     
